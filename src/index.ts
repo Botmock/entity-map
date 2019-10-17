cb0473098689bbd7e615ddad3bc0f93ecf917316
Kevin Donahue <nonnontrivial@gmail.com>
2019-10-17 14:01:13 -0400

    use lowercase keys in map

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 export { Entity } from "./types";
 
 export const map = new Map([
-  ["Age", { dialogflow: "@sys.age", amazon: "AMAZON.NUMBER" }],
-  ["Any", { dialogflow: "@sys.any", amazon: undefined }],
-  ["City", { dialogflow: "@sys.geo-city", amazon: "AMAZON.City" }],
-  ["Color", { dialogflow: "@sys.color", amazon: "AMAZON.Color" }],
-  ["Country", { dialogflow: "@sys.geo-country", amazon: "AMAZON.Country" }],
-  ["Currency", { dialogflow: "@sys.currency-name", amazon: undefined }],
-  ["Date", { dialogflow: "@sys.date", amazon: "AMAZON.DATE" }],
-  ["Dimension", { dialogflow: "@sys.unit-volume", amazon: undefined }],
-  ["Duration", { dialogflow: "@sys.duration", amazon: "AMAZON.DURATION" }],
-  ["Email", { dialogflow: "@sys.email", amazon: undefined }],
-  ["Name", { dialogflow: "@sys.given-name", amazon: undefined }],
-  ["Number", { dialogflow: "@sys.number", amazon: "AMAZON.NUMBER" }],
-  ["Ordinal", { dialogflow: "@sys.ordinal", amazon: "AMAZON.Ordinal" }],
-  ["Phone Number", { dialogflow: "@sys.phone-number", amazon: "AMAZON.PhoneNumber" }],
+  ["age", { dialogflow: "@sys.age", amazon: "AMAZON.NUMBER" }],
+  ["any", { dialogflow: "@sys.any", amazon: undefined }],
+  ["city", { dialogflow: "@sys.geo-city", amazon: "AMAZON.City" }],
+  ["color", { dialogflow: "@sys.color", amazon: "AMAZON.Color" }],
+  ["country", { dialogflow: "@sys.geo-country", amazon: "AMAZON.Country" }],
+  ["currency", { dialogflow: "@sys.currency-name", amazon: undefined }],
+  ["date", { dialogflow: "@sys.date", amazon: "AMAZON.DATE" }],
+  ["dimension", { dialogflow: "@sys.unit-volume", amazon: undefined }],
+  ["duration", { dialogflow: "@sys.duration", amazon: "AMAZON.DURATION" }],
+  ["email", { dialogflow: "@sys.email", amazon: undefined }],
+  ["name", { dialogflow: "@sys.given-name", amazon: undefined }],
+  ["number", { dialogflow: "@sys.number", amazon: "AMAZON.NUMBER" }],
+  ["ordinal", { dialogflow: "@sys.ordinal", amazon: "AMAZON.Ordinal" }],
+  ["phonenumber", { dialogflow: "@sys.phone-number", amazon: "AMAZON.PhoneNumber" }],
 ]);
 
 interface Options {
